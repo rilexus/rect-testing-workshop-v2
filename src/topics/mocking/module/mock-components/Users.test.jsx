b655e20008872af985072323829cb1680f4ac4c1
Stan <stanip@hotmail.de>
2020-11-25 18:41:28 +0100

    mock SomeHugeWrapper

--- src/topics/mocking/module/mock-components/Users.test.jsx
+++ src/topics/mocking/module/mock-components/Users.test.jsx
@@ -1,14 +1,16 @@
 import { render } from "@testing-library/react";
 import React from "react";
 import { Users } from "./Users";
+import { SomeHugeWrapper as MockedSomeHugeWrapper } from "./SomeHugeWrapper";
 
-jest.mock("./SomeHugeWrapper", () => {
-  return {
-    SomeHugeWrapper: ({ children }) => <div>{children}</div>,
-  };
-});
+jest.mock("./SomeHugeWrapper");
 
 describe("Resolve", () => {
+  beforeEach(() => {
+    MockedSomeHugeWrapper.mockImplementation(({ children }) => (
+      <div>children</div>
+    ));
+  });
   it("should show user name list", async function () {
     const { queryByTestId } = render(<Users id={"id"} />);
     const comp = queryByTestId("id");
